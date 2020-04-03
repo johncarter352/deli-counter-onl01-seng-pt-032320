df4c24975679195866133d062b05072d53bb39a7
John Carter <johncarter352@gmail.com>
2020-04-03 05:08:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+def line(array)
+  katz_deli = []
+  if array == 0 
+  puts "The line is currently empty."
+  else
+   puts "The line is currently #{katz_deli.length}.}"
+  end
+end
