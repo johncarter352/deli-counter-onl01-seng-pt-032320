155e2788830a78cf67e8b311a629cd9b6650646f
John Carter <johncarter352@gmail.com>
2020-04-03 05:12:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
     puts "The line is currently empty."
   else
     line_wait = "The line is currently"
-    deli.each.with_index(1) do |person, i|
+    array.each.with_index(1) do |person, i|
       line_wait << " #{i}. #{person}"
     end
     line_wait
